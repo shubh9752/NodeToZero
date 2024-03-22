96eb9a83f5dbd0ae7c8dc7b5d8442f2afa91cdfe
shubh9752 <shubhamkumar3.sk@gmail.com>
2024-03-22 18:00:18 +0530

    fs crud operations

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const fs=require('fs');
 // const { encode } = require('querystring');
 
 console.log(`starting`);
-const buffer=fs.readFileSync('data.txt',{encoding:'utf8'});
-console.log(buffer);
+try {
+    fs.writeFileSync('data.txt','khela oh jaye');
+    fs.appendFileSync('data.txt',' nahi hoga jaye');
+    const buffer=fs.readFileSync('data.txt',{encoding:'utf8'});
+    console.log(buffer);
+} catch (error) {
+    console.log(error)
+}
+fs.unlinkSync('ej.txt');
+
+
 console.log(`checking fs is blocking code`);
