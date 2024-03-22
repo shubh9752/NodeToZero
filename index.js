ac9f935b5f160cee96d2923376fe2e78e6708274
shubh9752 <shubhamkumar3.sk@gmail.com>
2024-03-22 19:43:38 +0530

    send mail through nodemailer

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
-const fs=require('fs');
-// const { encode } = require('querystring');
 
-console.log(`starting`);
-try {
-    //these are async operations so order is not fixedhere
-    fs.writeFile('data.txt','khela oh jaye',(err)=>{
-        if(err){
-            console.log(err);
+const nodeMailer=require('nodemailer');
+const {Gkey}=require('./secretKey')
+
+const sendMailer=async()=>{
+    //1 creating a email transporter
+    const transporter=nodeMailer.createTransport({
+        //selecting a smtp 
+        service:'gmail',
+        auth:{
+            user:'shubhamkumar3.sk@gmail.com',
+            pass:Gkey
         }
-        console.log("data added");
-    });
-    fs.appendFile('data.txt',' nahi hoga jaye',(err)=>{
-        if(err){
-            console.log(err);
-        }
-        console.log("data appended");
-    });
-    const buffer=fs.readFile('data.txt',(err,data)=>{
-        if(err){
-            console.log(err);
-        }
-        console.log(data.toString());
-    });
-    console.log(buffer);
-} catch (error) {
-    console.log(error)
-}
-fs.unlink('ejs.txt',(err)=>{
-    if(err){
-        console.log(err);
+    })
+    //2 configuring email content
+    const emailOptions={
+        from:'shubhamkumar3.sk@gmail.com',
+        to:'killersid581@yahoo.in',
+        subject:'Thankyou for subcribe',
+        text:'thankyou for subscribing us',
+        html:'<h1>thankyou for subscribing us</h1>'
+    };
+    try {
+        //3 sending the email
+        const info=await transporter.sendMail(emailOptions);
+        console.log(info+" sent successfuly");
+        
+    } catch (error) {
+        console.log("email not send",error);
     }
-    console.log('file deleted');
-});
 
-
-console.log(`proved non blocking code`);
+}
+sendMailer();
