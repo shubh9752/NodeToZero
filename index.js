31ff8752a3d3072ba5997b14e79f5f2e05ad3ad1
shubh9752 <shubhamkumar3.sk@gmail.com>
2024-03-22 17:44:12 +0530

    fs.readFileSync

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-const http = require('http');
-const server = http.createServer((req,res)=>{
-    console.log(req.url);
-    let url = req.url;
-    if(url==='/'){
-        res.write('<h1>you are on homepage</h1>');
-        res.end('home page end');
-    }else if(url==='/product'){
-        res.write('<h1>you are on product page</h1>');
-        res.end('product page end');
-    }
-    
-    res.end('server is shubham server');
-});
-server.listen(8080,()=>{
-    console.log('server running on 8080');
-});
+const fs=require('fs');
+// const { encode } = require('querystring');
+
+console.log(`starting`);
+const buffer=fs.readFileSync('data.txt',{encoding:'utf8'});
+console.log(buffer);
+console.log(`checking fs is blocking code`);
