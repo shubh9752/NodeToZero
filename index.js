a121c543ed10faca111138b3e14aaa1961903e66
shubh9752 <shubhamkumar3.sk@gmail.com>
2024-03-22 15:55:13 +0530

    handling multiple requests

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
+    console.log(req.url);
+    let url = req.url;
+    if(url==='/'){
+        res.write('<h1>you are on homepage</h1>');
+        res.end('home page end');
+    }else if(url==='/product'){
+        res.write('<h1>you are on product page</h1>');
+        res.end('product page end');
+    }
+    
     res.end('server is shubham server');
 });
 server.listen(8080,()=>{
